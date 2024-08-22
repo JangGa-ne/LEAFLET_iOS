20ea2ffd36edf3b939ae37aabe946f5b62a8ac61
JangGa-ne <devjang.001@gmail.com>
2024-08-22 17:50:49 +0900

    240822

---
+++ leaflet/viewController/DetailVC.swift
@@ -0,0 +1,57 @@
+//
+//  DetailVC.swift
+//  leaflet
+//
+//  Created by 제현 on 8/22/24.
+//
+
+import UIKit
+
+class DetailTC: UITableViewCell {
+    
+    
+}
+
+class DetailVC: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if #available(iOS 13.0, *) { return .darkContent } else { return .default }
+    }
+    
+    @IBAction func back_btn(sender: UIButton) { navigationController?.popViewController(animated: true) }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.separatorStyle = .none
+        tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        tableView.delegate = self; tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setBackSwipeGesture(true)
+    }
+}
+
+extension DetailVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailTC", for: indexPath) as! DetailTC
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+    }
+}
